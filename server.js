a0115b4c8e452d349a9aab2b17a56e0bf591dffb
Johan Boerema <johan@johanboerema.nl>
2018-02-23 19:27:33 +0100

    add project page

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -42,6 +42,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
   });
 });
 
